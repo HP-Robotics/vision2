27b05947be72459d50edaae34c26c8b282f1c072
Jeremy White <jwhite@codeweavers.com>
2016-04-20 20:19:02 -0500

    Refine the filters even more, based on research David and I did on 3,000 images from 10K.

--- vision.c
+++ vision.c
@@ -47,7 +47,7 @@
     int max_v;
     int apply_fancy_logic;
 */
-filter_t g_color_filter = { 36, 113, 118, 161, 0, 116, 0};
+filter_t g_color_filter = { 30, 150, 113, 161, 0, 116, 0};
 
 //filter_t g_color_filter = { 0, 255, 0, 255, 0, 100 };
 //{ 128, 255, 0, 255, 64, 255 };
